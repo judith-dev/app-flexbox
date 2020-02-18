542000c6b8d9d07d06a3a581a56bfb8651308a21
@judith-dev <judith.develop@gmail.com>
2020-02-17 22:44:42 -0600

    flexbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div>
+        <ul>
+            <li className="flex1"> one </li> 
+            <li className="flex2"> two </li> 
+            <li className="flex3"> three </li> 
+            <li className="flex4"> four </li> 
+            <li className="flex5"> five </li> 
+        </ul> 
+        </div>
+    );
 }
 
 export default App;
